10006adc24b210fc4f45ab1d661b98c797ad6f7e
yho4 <yho4@student.unimelb.edu.au>
2018-07-29 01:31:17 +1000

    initial commit for merging

--- app/src/main/java/com/example/zacharyho/myunihack/MainActivity.java
+++ app/src/main/java/com/example/zacharyho/myunihack/MainActivity.java
@@ -2,15 +2,42 @@ package com.example.zacharyho.myunihack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.PointF;
+import android.support.annotation.NonNull;
+
+import org.json.*;
+
+import com.google.gson.JsonElement;
+import com.mapbox.api.directions.v5.DirectionsCriteria;
+import com.mapbox.api.matrix.v1.MapboxMatrix;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.Point;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.LineString;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
+        MapboxMap.OnMapClickListener {
     private MapView mapView;
+    private Marker featureMarker;
+    private MapboxMap mapboxMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +48,62 @@ public class MainActivity extends AppCompatActivity {
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
 
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(MapboxMap mapboxMap) {
-                // One way to add a marker view
-                mapboxMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(-37.800449,144.963938))
-                        .title("Test1")
-                        .snippet("VIC")
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        MainActivity.this.mapboxMap = mapboxMap;
+        mapboxMap.addOnMapClickListener(this);
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng point) {
+        if (featureMarker != null) {
+            mapboxMap.removeMarker(featureMarker);
+        }
+
+        final PointF pixel = mapboxMap.getProjection().toScreenLocation(point);
+        List<Feature> features = mapboxMap.queryRenderedFeatures(pixel);
+
+        if (features.size() > 0) {
+            Feature feature = features.get(0);
+
+            String property;
+
+            StringBuilder stringBuilder = new StringBuilder();
+            if (feature.properties() != null) {
+                for (Map.Entry<String, JsonElement> entry : feature.properties().entrySet()) {
+                    stringBuilder.append(String.format("%s - %s", entry.getKey(), entry.getValue()));
+                    stringBuilder.append(System.getProperty("line.separator"));
+                }
+
+                featureMarker = mapboxMap.addMarker(new MarkerOptions()
+                        .position(point)
+                        .title(getString(R.string.query_feature_marker_title))
+                        .snippet(stringBuilder.toString())
+                );
+
+            } else {
+                property = getString(R.string.query_feature_marker_snippet);
+                featureMarker = mapboxMap.addMarker(new MarkerOptions()
+                        .position(point)
+                        .snippet(property)
                 );
             }
-        });
-
+        } else {
+            featureMarker = mapboxMap.addMarker(new MarkerOptions()
+                    .position(point)
+                    .snippet(getString(R.string.query_feature_marker_snippet))
+            );
+        }
+        mapboxMap.selectMarker(featureMarker);
     }
+
+
 
     @Override
     public void onStart() {
@@ -76,5 +146,46 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
     }
+
+    public String drawAll()  throws Exception{
+        String routeStr = "";
+
+        String httpsURL = createRequest("walking", "-37.802078,144.966681", "-37.800748,144.966905");
+        URL myUrl = new URL(httpsURL);
+        HttpsURLConnection conn = (HttpsURLConnection)myUrl.openConnection();
+        InputStream is = conn.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+
+        String inputLine;
+
+        while ((inputLine = br.readLine()) != null) {
+            routeStr = inputLine;
+        }
+
+        br.close();
+
+        JSONObject obj = new JSONObject(routeStr);
+        String coordinates = obj.getJSONObject("route").getString("coordinates");
+
+        JSONArray arr = obj.getJSONArray("posts");
+        for (int i = 0; i < arr.length(); i++)
+        {
+            String post_id = arr.getJSONObject(i).getString("post_id");
+        }
+        return routeStr;
+    }
+
+    private String createRequest(String profile, String coodinate1, String coodinate2) {
+        String key = "access_token=pk.eyJ1IjoicmRpYW9zdHVkZW50IiwiYSI6ImNqazU5Y3dxMDBtenMzcHBqdjlncWt5YTYifQ.K8YgUZPXPJ6xwuJrFoyM3g";
+
+        String httpStr = "https://api.mapbox.com/directions/v5/mapbox/";
+        httpStr = httpStr + coodinate1 + ";" + coodinate2 + "?geometries=geojson&" + key;
+
+        return httpStr;
+    }
+
+
+
 }
 
