5802a8eb774a9ffb4dc7298489f6579294970cf6
amterp <alexander.terp@gmail.com>
2018-07-28 22:58:29 +1000

    Add ability to read roads

--- app/src/main/java/com/example/nicholas/unihack_2018_1/algorithm/DataParser.java
+++ app/src/main/java/com/example/nicholas/unihack_2018_1/algorithm/DataParser.java
@@ -12,10 +12,18 @@ import java.util.Map;
 
 public class DataParser {
     private static final String MAP_KEY = "map";
+    private static final String POINTS_KEY = "points";
     private static final String ROADS_KEY = "roads";
     private static final String LATITUDE_KEY = "lat";
     private static final String LONGITUDE_KEY = "long";
     private static final String NEIGHBORS_KEY = "neighbours";
+
+    private static final String CRIME_KEY  = "crime";
+    private static final String ELEVATION_KEY  = "elevation";
+    private static final String LIGHTS_KEY  = "lights";
+    private static final String PARKS_KEY  = "parks";
+    private static final String NICENESS_KEY  = "usr-NICE";
+    private static final String SAFETY_KEY  = "usr-SAFE";
 
 
     public static Map<String, Intersection> intersections;
@@ -27,13 +35,14 @@ public class DataParser {
     public static void readData(DataSnapshot snapshot) {
         readIntersections(snapshot);
         readRoads(snapshot);
+        Log.d("Finished:", "Reading data");
     }
 
     private static void readIntersections(DataSnapshot snapshot) {
         intersections = new HashMap<>();
 
         // Grab list of points from snapshot.
-        Iterable<DataSnapshot> points = snapshot.child("map").child("points").getChildren();
+        Iterable<DataSnapshot> points = snapshot.child(MAP_KEY).child(POINTS_KEY).getChildren();
 
         // Iterate over points/intersections.
         int count = 0;
@@ -58,21 +67,35 @@ public class DataParser {
 
             intersections.put(newIntesection.getId(), newIntesection);
             count++;
-            Log.d("Num of points read in:", Integer.toString(count));
+            Log.d("Num of roads read in:", Integer.toString(count));
         }
 
-        Log.d("Finished!", "Yay");
+        Log.d("Finished:", "Reading intersections");
     }
 
     private static void readRoads(DataSnapshot snapshot) {
         roads = new HashMap<>();
 
-        // Grab list of points from snapshot.
-        Iterable<DataSnapshot> roads = snapshot.child(MAP_KEY).child(ROADS_KEY).getChildren();
+        // Grab list of roads from snapshot.
+        Iterable<DataSnapshot> roadList = snapshot.child(MAP_KEY).child(ROADS_KEY).getChildren();
 
-        // Iterate over points/intersections.
+        // Iterate over roads.
         int count = 0;
-        while (roads.iterator().hasNext()) {
-            DataSnapshot nextIntersection = roads.iterator().next();
+        while (roadList.iterator().hasNext()) {
+            DataSnapshot nextRoad = roadList.iterator().next();
+            final float crime = Float.parseFloat((String) nextRoad.child(CRIME_KEY).getValue());
+            final float elevation = Float.parseFloat((String) nextRoad.child(ELEVATION_KEY).getValue());
+            final float lights = Float.parseFloat((String) nextRoad.child(LIGHTS_KEY).getValue());
+            final float parks = Float.parseFloat((String) nextRoad.child(PARKS_KEY).getValue());
+            final float niceness = Float.parseFloat((String) nextRoad.child(NICENESS_KEY).getValue());
+            final float safety = Float.parseFloat((String) nextRoad.child(SAFETY_KEY).getValue());
+
+            final RoadInfo newRoadInfo = new RoadInfo(crime, elevation, lights, parks, niceness, safety);
+
+            roads.put(nextRoad.getKey(), newRoadInfo);
+            count++;
+            Log.d("Num of points read in:", Integer.toString(count));
+        }
+        Log.d("Finished:", "Reading roads");
     }
 }
